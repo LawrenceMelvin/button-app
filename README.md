0a76cc123d0f0c647c5a5e4d26c8428dabc23a6a
LawrenceMelvin <53090937+LawrenceMelvin@users.noreply.github.com>
2021-04-29 12:24:04 +0530

    formatting docunment

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # button-app
  This app contains a lot of button that contains calculator and many random buttons .
+The Monthly Expense and prime or not button is not fully completed 
--- src/components/Liveclock.js
+++ src/components/Liveclock.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import Clock from 'react-live-clock';
-import './Formstyle.css'
+import React, { Component } from "react";
+import Clock from "react-live-clock";
+import "./Formstyle.css";
 
 export class Liveclock extends Component {
-    render() {
-        return (
-            <div className="formDetails">
-                <div className="clk">  
-                    <Clock format={'HH:mm:ss'} ticking={true} />  
-                </div>  
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="formDetails">
+        <div className="clk">
+          <Clock format={"HH:mm:ss"} ticking={true} />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Liveclock
+export default Liveclock;
